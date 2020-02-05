69edab2a13b582effd29323805eef9b03e46df34
RachelFurner <raf59@cam.ac.uk>
2020-02-05 16:10:58 +0000

    Fully vectorised iteration scheme. Code now runs much much faster. Tested against previous set up - results are identical when comparing plot of 12 iterations, and two randomly chosen printed samples from 'inputs'.

--- LinearRegressionModel/IterativelyForecast.py
+++ LinearRegressionModel/IterativelyForecast.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
+from skimage.util import view_as_windows
 import os
 import xarray as xr
 import pickle
@@ -80,74 +81,111 @@ def interator(exp_name, model, init, ch_start, num_steps, ds):
     input_std  = np.array(input_std).astype(float)
     input_mean = input_mean.reshape(1,input_mean.shape[0])
     input_std  = input_std.reshape(1,input_std.shape[0])
-    
-    out_t   = np.zeros((z_size-7, y_size-7, x_size-7))
-    out_tm1 = np.zeros((z_size-7, y_size-7, x_size-7))
-    out_tm2 = np.zeros((z_size-7, y_size-7, x_size-7))
+  
+    # Set region to predict for - we want to exclude boundary points, and near to boundary points 
+    x_lw = 3
+    x_up = x_size-4 
+    y_lw = 3
+    y_up = y_size-4 
+    z_lw = 3
+    z_up = z_size-4 
+    x_subsize = x_size -7
+    y_subsize = y_size -7
+    z_subsize = z_size -7
 
-    predictions = np.zeros((num_steps, z_size, y_size, x_size))
+    out_t   = np.zeros((z_subsize, y_subsize, x_subsize))
+    out_tm1 = np.zeros((z_subsize, y_subsize, x_subsize))
+    out_tm2 = np.zeros((z_subsize, y_subsize, x_subsize))
+
+    predictions = np.empty((num_steps, z_size, y_size, x_size))
+    predictions[:,:,:,:] = np.nan
     print('predictions.shape ; ' + str(predictions.shape))
     # Set initial conditions to match init
     predictions[0,:,:,:] = init[:,:,:]
     # Set all boundary and near land data to match 'da_T'
-    predictions[1:,:,:,0:3]             = da_T[1:num_steps,:,:,0:3]
-    predictions[1:,:,:,x_size-4:x_size] = da_T[1:num_steps,:,:,x_size-4:x_size]
-    predictions[1:,:,0:3,:]             = da_T[1:num_steps,:,0:3,:]
-    predictions[1:,:,y_size-4:y_size,:] = da_T[1:num_steps,:,y_size-4:y_size,:]
-    predictions[1:,0:3,:,:]             = da_T[1:num_steps,0:3,:,:]
-    predictions[1:,z_size-4:z_size,:,:] = da_T[1:num_steps,z_size-4:z_size,:,:]
+    predictions[1:,:,:,0:x_lw]      = da_T[1:num_steps,:,:,0:x_lw]
+    predictions[1:,:,:,x_up:x_size] = da_T[1:num_steps,:,:,x_up:x_size]
+    predictions[1:,:,0:y_lw,:]      = da_T[1:num_steps,:,0:y_lw,:]
+    predictions[1:,:,y_up:y_size,:] = da_T[1:num_steps,:,y_up:y_size,:]
+    predictions[1:,0:z_lw,:,:]      = da_T[1:num_steps,0:z_lw,:,:]
+    predictions[1:,z_up:z_size,:,:] = da_T[1:num_steps,z_up:z_size,:,:]
+
 
     for t in range(1,num_steps):
-        inputs = []
+
         out_tm2[:,:,:] = out_tm1[:,:,:]
         out_tm1[:,:,:] = out_t[:,:,:]
         print(t)
-        for x in range(3,x_size-4):
-            for y in range(3,y_size-4):
-                for z in range(3,z_size-4):
-                   input_temp = []
-                   if run_vars['dimension'] == 2:
-                      input_temp = input_temp + predictions[t-1,z,y-1:y+2,x-1:x+2].reshape((-1)).tolist()
-                      if run_vars['sal']:
-                         input_temp = input_temp + da_S[t-1,z,y-1:y+2,x-1:x+2].values.reshape((-1)).tolist()
-                      if run_vars['current']:
-                         input_temp = input_temp + da_U[t-1,z,y-1:y+2,x-1:x+2].values.reshape((-1)).tolist()
-                         input_temp = input_temp + da_V[t-1,z,y-1:y+2,x-1:x+2].values.reshape((-1)).tolist()
-                   elif run_vars['dimension'] == 3:
-                      input_temp = input_temp + predictions[t-1,z-1:z+2,y-1:y+2,x-1:x+2].reshape((-1)).tolist()
-                      if run_vars['sal']:
-                         input_temp = input_temp + da_S[t-1,z-1:z+2,y-1:y+2,x-1:x+2].values.reshape((-1)).tolist()
-                      if run_vars['current']:
-                         input_temp = input_temp + da_U[t-1,z-1:z+2,y-1:y+2,x-1:x+2].values.reshape((-1)).tolist()
-                         input_temp = input_temp + da_V[t-1,z-1:z+2,y-1:y+2,x-1:x+2].values.reshape((-1)).tolist()
-                   if run_vars['eta']:
-                      input_temp = input_temp + da_eta[t-1,y-1:y+2,x-1:x+2].values.reshape((-1)).tolist()
-                   if run_vars['lat']:
-                      input_temp.append(da_lat[y].values)
-                   if run_vars['dep']:
-                      input_temp.append(da_depth[z].values)
-  
-                   inputs.append(input_temp)
 
-        # convert to array
-        inputs = np.array(inputs)
+        if run_vars['dimension'] == 2:
+           temp = view_as_windows(predictions[t-1,z_lw:z_up,y_lw-1:y_up+1,x_lw-1:x_up+1], (1,3,3), 1)
+           inputs = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+           if run_vars['sal']:
+              temp = view_as_windows(da_S[t-1,z_lw:z_up,y_lw-1:y_up+1,x_lw-1:x_up+1].values, (1,3,3), 1)
+              temp = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+              inputs = np.concatenate( (inputs, temp), axis=-1) 
+           if run_vars['current']:
+              temp = view_as_windows(da_U[t-1,z_lw:z_up,y_lw-1:y_up+1,x_lw-1:x_up+1].values, (1,3,3), 1)
+              temp = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+              inputs = np.concatenate( (inputs, temp), axis=-1) 
+              temp = view_as_windows(da_V[t-1,z_lw:z_up,y_lw-1:y_up+1,x_lw-1:x_up+1].values, (1,3,3), 1)
+              temp = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+              inputs = np.concatenate( (inputs, temp), axis=-1) 
+        elif run_vars['dimension'] == 3:
+           temp = view_as_windows(predictions[t-1,z_lw-1:z_up+1,y_lw-1:y_up+1,x_lw-1:x_up+1], (3,3,3), 1)
+           inputs = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1))
+           if run_vars['sal']:
+              temp = view_as_windows(da_S[t-1,z_lw-1:z_up+1,y_lw-1:y_up+1,x_lw-1:x_up+1].values, (3,3,3), 1)
+              temp = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+              inputs = np.concatenate( (inputs, temp), axis=-1) 
+           if run_vars['current']:
+              temp = view_as_windows(da_U[t-1,z_lw-1:z_up+1,y_lw-1:y_up+1,x_lw-1:x_up+1].values, (3,3,3), 1)
+              temp = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+              inputs = np.concatenate( (inputs, temp), axis=-1) 
+              temp = view_as_windows(da_V[t-1,z_lw-1:z_up+1,y_lw-1:y_up+1,x_lw-1:x_up+1].values, (3,3,3), 1)
+              temp = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+              inputs = np.concatenate( (inputs, temp), axis=-1) 
+        else:
+           print('ERROR, dimension neither 2 nor 3')
+        if run_vars['eta']:
+           temp = view_as_windows(da_eta[t-1,y_lw-1:y_up+1,x_lw-1:x_up+1].values, (3,3), 1)
+           temp = np.tile(temp, (z_subsize,1,1,1,1))
+           temp = temp.reshape((temp.shape[0], temp.shape[1], temp.shape[2], -1)) 
+           inputs = np.concatenate( (inputs, temp), axis=-1) 
+        if run_vars['lat']:
+           temp = da_lat[y_lw:y_up].values
+           # convert to 3d shape, plus additional dim of 1 for feature.
+           temp = np.tile(temp, (z_subsize,1))
+           temp = np.expand_dims(temp, axis=-1)
+           temp = np.tile(temp, (1,x_subsize))
+           temp = np.expand_dims(temp, axis=-1)
+           inputs = np.concatenate( (inputs, temp), axis=-1) 
+        if run_vars['dep']:
+           temp = da_depth[z_lw:z_up].values
+           # convert to 3d shape, plus additional dim of 1 for feature.
+           temp = np.expand_dims(temp, axis=-1)
+           temp = np.tile(temp, (1,y_subsize))
+           temp = np.expand_dims(temp, axis=-1)
+           temp = np.tile(temp, (1,x_subsize))
+           temp = np.expand_dims(temp, axis=-1)
+           inputs = np.concatenate( (inputs, temp), axis=-1) 
 
-        print(inputs.shape)
+        # reshape from grid (z,y,x,features) to list (no_points, features)
+        inputs = inputs.reshape((z_subsize*y_subsize*x_subsize,inputs.shape[-1]))
+ 
         if run_vars['poly_degree'] > 1: 
            # Add polynomial combinations of the features
            polynomial_features = PolynomialFeatures(degree=run_vars['poly_degree'], interaction_only=True, include_bias=False)
            inputs = polynomial_features.fit_transform(inputs)
-        print(inputs.shape)
 
         # normalise inputs
         inputs = np.divide( np.subtract(inputs, input_mean), input_std)
-        print(inputs.shape)
                  
         # predict and then de-normalise outputs
         out_temp = model.predict(inputs) * output_std + output_mean
 
         # reshape out
-        out_t = out_temp.reshape((z_size-7, y_size-7, x_size-7))
+        out_t = out_temp.reshape((z_subsize, y_subsize, x_subsize))
 
         if t==0: 
            deltaT = out_t
@@ -156,7 +194,7 @@ def interator(exp_name, model, init, ch_start, num_steps, ds):
         if t>1: 
            deltaT = (23.0/12.0)*out_t-(4.0/3.0)*out_tm1+(5.0/12.0)*out_tm2
         
-        predictions[t,3:z_size-4,3:y_size-4,3:x_size-4] = predictions[t-1,3:z_size-4,3:y_size-4,3:x_size-4] + ( deltaT )
+        predictions[ t, z_lw:z_up, y_lw:y_up, x_lw:x_up ] = predictions[ t-1, z_lw:z_up, y_lw:y_up, x_lw:x_up ] + ( deltaT )
 
     return predictions
 
@@ -165,7 +203,8 @@ def interator(exp_name, model, init, ch_start, num_steps, ds):
 #-----------------------------------------------
 print('reading in ds')
 DIR  = '/data/hpcdata/users/racfur/MITGCM_OUTPUT/20000yr_Windx1.00_mm_diag/'
-data_filename=DIR+'cat_tave_5000yrs_SelectedVars.nc'
+#data_filename=DIR+'cat_tave_5000yrs_SelectedVars.nc'
+data_filename=DIR+'cat_tave_500yrs_SelectedVars.nc'
 ds   = xr.open_dataset(data_filename)
 da_T=ds['Ttave'][start:start+for_len_yrs*12]   
 
@@ -175,14 +214,6 @@ x_size = da_T.shape[3]
 
 print('da_T.shape')
 print(da_T.shape)
-
-#---------------------------
-# Set persistence forecasts
-#---------------------------
-print('Set perstistence forecast')
-persistence = np.zeros((for_len_yrs*12,len(x_points)))  # assume 1 month forecast period - doesn't matter if this is actually too long
-for point in range(len(x_points)):
-   persistence[:,point] = da_T[0,z_points[point],y_points[point],x_points[point]]
 
 #----------------------
 # Make the predictions
@@ -210,6 +241,14 @@ if iteratively_predict:
 predictions = np.load(pred_filename)
 print('predictions.shape')
 print(predictions.shape)
+
+#---------------------------
+# Set persistence forecasts
+#---------------------------
+print('Set perstistence forecast')
+persistence = np.zeros((for_len_yrs*12,len(x_points)))  # assume 1 month forecast period - doesn't matter if this is actually too long
+for point in range(len(x_points)):
+   persistence[:,point] = da_T[0,z_points[point],y_points[point],x_points[point]]
 
 #---------------------------------------------------------------------------
 # Plot the predictions against da_T and persistence for a variety of points
