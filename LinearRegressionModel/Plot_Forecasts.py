17ac7b29062fb744b73fc7ada0e727d477167e72
RachelFurner <raf59@cam.ac.uk>
2020-02-13 16:06:52 +0000

    updates to plotting routines - added timeseries plots

--- LinearRegressionModel/Plot_Forecasts.py
+++ LinearRegressionModel/Plot_Forecasts.py
@@ -16,30 +16,14 @@ import os
 import xarray as xr
 import pickle
 
-#----------------------------
-# Set variables for this run
-#----------------------------
-
-for_len_yrs = 100    # forecast length in years
-
-x_points=[5,5,5]
-y_points=[5,10,15]
-z_points=[3,3,3]
-
-start = 200
-for_len = int(for_len_yrs * 12)
-
 #-----------------------------------------------
 # Read in netcdf file for 'truth' and get shape
 #-----------------------------------------------
 print('reading in ds')
 DIR  = '/data/hpcdata/users/racfur/MITGCM_OUTPUT/20000yr_Windx1.00_mm_diag/'
-if for_len_yrs < 500:
-   data_filename=DIR+'cat_tave_500yrs_SelectedVars.nc'
-else:
-   data_filename=DIR+'cat_tave_5000yrs_SelectedVars.nc'
+data_filename=DIR+'cat_tave_5000yrs_SelectedVars.nc'
 ds = xr.open_dataset(data_filename)
-da_T=ds['Ttave'][start:start+for_len_yrs*12]   
+da_T=ds['Ttave'][:12001,:,:,:]
 
 z_size = da_T.shape[1]
 y_size = da_T.shape[2]
@@ -54,31 +38,25 @@ print(da_T.shape)
 
 pred_dir = '/data/hpcdata/users/racfur/DynamicPrediction/RegressionOutputs/ITERATED_PREDICTION_ARRAYS/'
 
-predictions1 = np.zeros((for_len+1, z_size, y_size, x_size))
 pred1_filename = pred_dir+ 'lr_3dLatDepUVSalEtaPolyDeg2_predictions.npy'
 predictions1 = np.load(pred1_filename)
 
-predictions2 = np.zeros((for_len+1, z_size, y_size, x_size))
 pred2_filename = pred_dir+ 'lr_2dLatDepUVSalEtaPolyDeg2_predictions.npy'
 predictions2 = np.load(pred2_filename)
 
-predictions3 = np.zeros((for_len+1, z_size, y_size, x_size))
 pred3_filename = pred_dir+ 'lr_3dDepUVSalEtaPolyDeg2_predictions.npy'
 predictions3 = np.load(pred3_filename)
 
-predictions4 = np.zeros((for_len+1, z_size, y_size, x_size))
 pred4_filename = pred_dir+ 'lr_3dLatUVSalEtaPolyDeg2_predictions.npy'
 predictions4 = np.load(pred4_filename)
 
-predictions5 = np.zeros((for_len+1, z_size, y_size, x_size))
 pred5_filename = pred_dir+ 'lr_3dLatDepSalEtaPolyDeg2_predictions.npy'
 predictions5 = np.load(pred5_filename)
 
-predictions6 = np.zeros((for_len+1, z_size, y_size, x_size))
 pred6_filename = pred_dir+ 'lr_3dLatDepUVEtaPolyDeg2_predictions.npy'
 predictions6 = np.load(pred6_filename)
 
-predictions7 = np.zeros((for_len+1, z_size, y_size, x_size))
+#predictions7 = np.zeros((for_len+1, z_size, y_size, x_size))
 pred7_filename = pred_dir+ 'lr_3dLatDepUVSalPolyDeg2_predictions.npy'
 predictions7 = np.load(pred7_filename)
 
@@ -86,91 +64,52 @@ predictions7 = np.load(pred7_filename)
 # Set persistence forecasts
 #---------------------------
 print('Set perstistence forecast')
-persistence = np.zeros((for_len_yrs*12,len(x_points)))  # assume 1 month forecast period - doesn't matter if this is actually too long
-for point in range(len(x_points)):
-   persistence[:,point] = da_T[0,z_points[point],y_points[point],x_points[point]]
+persistence = np.zeros((12000, z_size, y_size, x_size))
+persistence[:,:,:,:] = da_T[0,:,:,:]
 
-#-------------------------
-# Plot spatial plots....
-#-------------------------
-fig, ax, im = rfplt.plot_depth_fld(da_T[0,:,:,:], 5, title=None, min_value=None, max_value=None)
-plt.show()
+#--------------------------
+# Plot spatial depth plots
+#--------------------------
+#fig, ax, im = rfplt.plot_depth_fld(predictions1[1,:,:,:], 'Temperature Predictions', 5, title=None, min_value=None, max_value=None)
+#plt.show()
 
-#-------------------------
-# Plot spatial plots....
-#-------------------------
-fig = rfplt.plot_depth_fld_diff(da_T[0,:,:,:], da_T[5,:,:,:], 5, title=None)
-plt.show()
+#-------------------------------
+# Plot spatial depth diff plots
+#-------------------------------
+#fig = rfplt.plot_depth_fld_diff(da_T[1,:,:,:], 'GCM Temperature', predictions1[1,:,:,:], 'Temperature Predictions', 5, title=None)
+#plt.show()
+
+#-----------------------
+# Plot y-cross sections
+#-----------------------
+#fig, ax, im = rfplt.plot_y_crss_sec(predictions1[1,:,:,:], 'Temperature Predictions', 5, title=None, min_value=None, max_value=None)
+#plt.show()
+
+#---------------------------------
+# Plot y-cross section diff plots
+#---------------------------------
+#fig = rfplt.plot_y_crss_sec_diff(da_T[1,:,:,:], 'GCM Temperature', predictions1[1,:,:,:],'Temperature Predictions', 5, title=None)
+#plt.show()
+
+#-----------------------
+# Plot x-cross sections
+#-----------------------
+#fig, ax, im = rfplt.plot_x_crss_sec(predictions1[1,:,:,:], 'Temperature Predictions', 5, title=None, min_value=None, max_value=None)
+#plt.show()
+
+#---------------------------------
+# Plot x-cross section diff plots
+#---------------------------------
+#fig = rfplt.plot_x_crss_sec_diff(da_T[1,:,:,:], 'GCM Temperature', predictions1[1,:,:,:],'Temperature Predictions', 5, title=None)
+#plt.show()
 
 #---------------------------------------------------------------------------
 # Plot the predictions against da_T and persistence for a variety of points
 #---------------------------------------------------------------------------
-for point in range(len(x_points)):
-   x = x_points[point]
-   y = y_points[point]
-   z = z_points[point]
 
-   fig = plt.figure(figsize=(15 ,8))
+fig = rfplt.plt_timeseries([5,5,5], 120, {'da_T (truth)':da_T.data, 'persistence':persistence, 'full_lr_model':predictions1, '2d':predictions2, 'no_Lat':predictions3, 'no_depth':predictions4, 'no_currents':predictions5, 'no_eta':predictions5, 'no_sal':predictions5})
+plt.show()
 
-   ax1=plt.subplot(311)
-   plt_len=int(10 * 12)
-   ax1.plot(da_T[:plt_len,z,y,x])
-   bot, top = ax1.get_ylim()
-   ax1.plot(persistence[:plt_len,point])
-   ax1.plot(predictions1[:plt_len,z,y,x])
-   ax1.plot(predictions2[:plt_len,z,y,x])
-   ax1.plot(predictions3[:plt_len,z,y,x])
-   ax1.plot(predictions4[:plt_len,z,y,x])
-   ax1.plot(predictions5[:plt_len,z,y,x])
-   ax1.plot(predictions6[:plt_len,z,y,x])
-   ax1.plot(predictions7[:plt_len,z,y,x])
-   #ax1.set_ylim(9, 9.4)
-   ax1.set_ylim(bot-0.1*abs(bot), top+0.1*abs(top))
-   ax1.set_ylabel('Temperature')
-   ax1.set_xlabel('No of months')
-   ax1.set_title('10 years')
-   ax1.legend(['da_T (truth)', 'persistence', 'full_lr_model', '2d', 'no_Lat', 'no_depth', 'no_currents', 'no_sal', 'no_eta']) 
-
-   ax2=plt.subplot(312)
-   plt_len=int(100 * 12)
-   ax2.plot(da_T[:plt_len,z,y,x])
-   bot, top = ax2.get_ylim()
-   ax2.plot(persistence[:plt_len,point])
-   ax2.plot(predictions1[:plt_len,z,y,x])
-   ax2.plot(predictions2[:plt_len,z,y,x])
-   ax2.plot(predictions3[:plt_len,z,y,x])
-   ax2.plot(predictions4[:plt_len,z,y,x])
-   ax2.plot(predictions5[:plt_len,z,y,x])
-   ax2.plot(predictions6[:plt_len,z,y,x])
-   ax2.plot(predictions7[:plt_len,z,y,x])
-   ax2.set_ylabel('Temperature')
-   ax2.set_xlabel('No of forecast steps')
-   ax2.set_ylim(9, 9.4)
-   ax2.set_title('100 years')
-   ax2.legend(['da_T (truth)', 'persistence', 'full_lr_model', '2d', 'no_Lat', 'no_depth', 'no_currents', 'no_sal', 'no_eta']) 
-
-   ax3=plt.subplot(313)
-   plt_len=for_len
-   ax3.plot(da_T[:plt_len,z,y,x])
-   bot, top = ax3.get_ylim()
-   ax3.plot(persistence[:plt_len,point])
-   ax3.plot(predictions1[:plt_len,z,y,x])
-   ax3.plot(predictions2[:plt_len,z,y,x])
-   ax3.plot(predictions3[:plt_len,z,y,x])
-   ax3.plot(predictions4[:plt_len,z,y,x])
-   ax3.plot(predictions5[:plt_len,z,y,x])
-   ax3.plot(predictions6[:plt_len,z,y,x])
-   ax3.plot(predictions7[:plt_len,z,y,x])
-   ax3.set_ylabel('Temperature')
-   ax3.set_xlabel('No of months')
-   ax3.set_ylim(9, 9.4)
-   ax3.set_title(str(for_len_yrs)+' years')
-   ax3.legend(['da_T (truth)', 'persistence', 'full_lr_model', '2d', 'no_Lat', 'no_depth', 'no_currents', 'no_sal', 'no_eta']) 
-
-   #plt.subplots_adjust(hspace=0.4, top=0.9, bottom=0.12, left=0.08, right=0.85)
-   plt.subplots_adjust(hspace=0.4)
-   #plt.suptitle('Timeseries at point '+str(x)+','+str(y)+','+str(z)+' from the simulator (da_T),\n and as predicted by the regression model, and persistence', fontsize=15, x=0.47, y=0.98)
-
-   plt.savefig('/data/hpcdata/users/racfur/DynamicPrediction/RegressionOutputs/PLOTS/multimodel_'+str(x)+'.'+str(y)+'.'+str(z)+'TruthvsPredict_Timeseries.png', bbox_inches = 'tight', pad_inches = 0.1)
-   plt.close()
+fig = rfplt.plt_3_timeseries([5,5,5], 120, 1200, 12000, {'da_T (truth)':da_T.data, 'persistence':persistence, 'full_lr_model':predictions1, '2d':predictions2, 'no_Lat':predictions3, 'no_depth':predictions4, 'no_currents':predictions5, 'no_eta':predictions5, 'no_sal':predictions5})
+plt.show()
 
---
+++ Tools/Model_Plotting.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Script to contain modules to  plots cross sections, time series etc from netcdf files 
+# of the sector configuration (created through MITGCM or NN/LR methods)
+
+# All modules expect the field for plotting to be passed (rather than a filename etc)
+
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+from matplotlib.cm import get_cmap
+from scipy.stats import skew
+
+#################################
+# Plotting spatial depth fields #
+#################################
+
+def plot_depth_ax(ax, field, level, min_value=None, max_value=None, cmap=None):
+
+    # Assumes field is (z, y, x)
+    if not min_value:
+       min_value = np.amin(field[level,:,:])  # Lowest value
+    # Need to work this bit out....
+    #if var[0] == 'S':
+    #    min_value = 33.5
+    if not max_value:
+       max_value = np.amax(field[level,:,:])   # Highest value
+
+    im = ax.pcolormesh(field[level,:,:], vmin=min_value, vmax=max_value, cmap=cmap)
+    ax.set_xlabel('x (\'longitude\')')
+    ax.set_ylabel('y (\'latitude\')')
+    
+    return(ax, im)
+
+def plot_depth_fld(field, field_name, level, title=None, min_value=None, max_value=None):
+    
+    # Create a figure
+    fig = plt.figure(figsize=(5,9))
+    ax = plt.subplot(111)
+    ax, im = plot_depth_ax(ax, field, level, min_value, max_value)
+
+    ax.set_title(str(field_name)+' at z='+str(level))
+
+    # Add a color bar
+    cb=plt.colorbar(im, ax=(ax), shrink=0.9, anchor=(0.5, 1.3))
+
+    if title:
+       plt.suptitle(title, fontsize=14)
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.01,hspace=0.01)
+
+    return(fig, ax, im)
+
+def plot_depth_fld_diff(field1, field1_name, field2, field2_name, level, title=None):
+    
+    flds_min_value = min( np.amin(field1[level,:,:]), np.amin(field2[level,:,:]) )
+    flds_max_value = max( np.amax(field1[level,:,:]), np.amax(field2[level,:,:]) )
+
+    diff_min_value = -max( abs(np.amin(field1[level,:,:]-field2[level,:,:])), abs(np.amax(field1[level,:,:]-field2[level,:,:])) )
+    diff_max_value =  max( abs(np.amin(field1[level,:,:]-field2[level,:,:])), abs(np.amax(field1[level,:,:]-field2[level,:,:])) )
+ 
+    fig = plt.figure(figsize=(15,9))
+    ax1 = plt.subplot(131)
+    ax2 = plt.subplot(132)
+    ax3 = plt.subplot(133)
+    ax1, im1 = plot_depth_ax(ax1, field1, level, flds_min_value, flds_max_value)
+    ax2, im2 = plot_depth_ax(ax2, field2, level, flds_min_value, flds_max_value)
+    ax3, im3 = plot_depth_ax(ax3, field1-field2, level, diff_min_value, diff_max_value, cmap='bwr')
+
+    ax1.set_title(str(field1_name)+' at z='+str(level))
+    ax2.set_title(str(field2_name)+' at z='+str(level))
+    ax3.set_title('The Difference')
+
+    cb1axes = fig.add_axes([0.05, 0.035, 0.60, 0.03]) 
+    cb3axes = fig.add_axes([0.73, 0.035, 0.24, 0.03]) 
+    cb1=plt.colorbar(im1, ax=(ax1,ax2), cax=cb1axes, orientation='horizontal')
+    cb3=plt.colorbar(im3, ax=ax3, cax=cb3axes, orientation='horizontal')
+
+    if title:
+       plt.suptitle(title, fontsize=14)
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.2, hspace = 0.01, bottom=0.15)
+
+    return(fig)
+
+##########################################################################################
+## Plot cross sections for y=const at a few discrete time points - start, middle and end #
+##########################################################################################
+
+def plot_y_crss_sec_ax(ax, field, y, min_value=None, max_value=None, cmap=None):
+
+    # Assumes field is (z, y, x)
+
+    if not min_value:
+       min_value = np.amin(field[:,y,:])  # Lowest value
+    # Need to work this bit out....
+    #if var[0] == 'S':
+    #    min_value = 33.5
+    if not max_value:
+       max_value = np.amax(field[:,y,:])   # Highest value
+
+    im = ax.pcolormesh(field[:,y,:], vmin=min_value, vmax=max_value, cmap=cmap)
+    ax.invert_yaxis()
+    ax.set_xlabel('x (\'longitude\')')
+    ax.set_ylabel('z (\'depth\')')
+    
+    return(ax, im)
+
+def plot_y_crss_sec(field, field_name, y, title=None, min_value=None, max_value=None):
+    
+    # Create a figure
+    fig = plt.figure(figsize=(9,5))
+    ax = plt.subplot(111)
+    ax, im = plot_y_crss_sec_ax(ax, field, y, min_value, max_value)
+
+    ax.set_title(str(field_name)+' at y='+str(y))
+
+    # Add a color bar
+    cb=plt.colorbar(im, ax=(ax), shrink=0.9)
+    
+    if title:
+       plt.suptitle(title, fontsize=14)
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.01,hspace=0.01)
+
+    return(fig, ax, im)
+
+def plot_y_crss_sec_diff(field1, field1_name, field2, field2_name, y, title=None):
+    
+    flds_min_value = min( np.amin(field1[:,y,:]), np.amin(field2[:,y,:]) )
+    flds_max_value = max( np.amax(field1[:,y,:]), np.amax(field2[:,y,:]) )
+
+    diff_min_value = -max( abs(np.amin(field1[:,y,:]-field2[:,y,:])), abs(np.amax(field1[:,y,:]-field2[:,y,:])) )
+    diff_max_value =  max( abs(np.amin(field1[:,y,:]-field2[:,y,:])), abs(np.amax(field1[:,y,:]-field2[:,y,:])) )
+ 
+    fig = plt.figure(figsize=(9,15))
+    ax1 = plt.subplot(311)
+    ax2 = plt.subplot(312)
+    ax3 = plt.subplot(313)
+    ax1, im1 = plot_y_crss_sec_ax(ax1, field1, y, flds_min_value, flds_max_value)
+    ax2, im2 = plot_y_crss_sec_ax(ax2, field2, y, flds_min_value, flds_max_value)
+    ax3, im3 = plot_y_crss_sec_ax(ax3, field1-field2, y, diff_min_value, diff_max_value, cmap='bwr')
+
+    ax1.set_title(str(field1_name)+' at y='+str(y))
+    ax2.set_title(str(field2_name)+' at y='+str(y))
+    ax3.set_title('The Difference')
+
+    cb1axes = fig.add_axes([0.92, 0.42, 0.03, 0.52]) 
+    cb3axes = fig.add_axes([0.92, 0.08, 0.03, 0.20]) 
+    cb1=plt.colorbar(im1, ax=(ax1,ax2), orientation='vertical', cax=cb1axes)
+    cb3=plt.colorbar(im3, ax=ax3, orientation='vertical', cax=cb3axes)
+ 
+    if title:
+       plt.suptitle(title, fontsize=14)
+
+    plt.tight_layout()
+    plt.subplots_adjust(hspace = 0.5, right=0.9, bottom=0.07, top=0.95)
+
+    return(fig)
+
+#######################################################
+# Plot cross section at x=const (i.e. North to South) #   
+#######################################################
+
+def plot_x_crss_sec_ax(ax, field, x, min_value=None, max_value=None, cmap=None):
+
+    # Assumes field is (z, y, x)
+
+    if not min_value:
+       min_value = np.amin(field[:,:,x])  # Lowest value
+    # Need to work this bit out....
+    #if var[0] == 'S':
+    #    min_value = 33.5
+    if not max_value:
+       max_value = np.amax(field[:,:,x])   # Highest value
+
+    im = ax.pcolormesh(field[:,:,x], vmin=min_value, vmax=max_value, cmap=cmap)
+    ax.invert_yaxis()
+    ax.invert_xaxis()
+    ax.set_xlabel('y (\'latitude\')')
+    ax.set_ylabel('z (\'depth\')')
+    
+    return(ax, im)
+
+def plot_x_crss_sec(field, field_name, x, title=None, min_value=None, max_value=None):
+    
+    # Create a figure
+    fig = plt.figure(figsize=(9,5))
+    ax = plt.subplot(111)
+    ax, im = plot_x_crss_sec_ax(ax, field, x, min_value, max_value)
+
+    ax.set_title(str(field_name)+' at x='+str(x))
+
+    # Add a color bar
+    cb=plt.colorbar(im, ax=(ax), shrink=0.9)
+    
+    if title:
+       plt.suptitle(title, fontsize=14)
+
+    plt.tight_layout()
+    plt.subplots_adjust(wspace=0.01,hspace=0.01)
+
+    return(fig, ax, im)
+
+def plot_x_crss_sec_diff(field1, field1_name, field2, field2_name, x, title=None):
+    
+    flds_min_value = min( np.amin(field1[:,:,x]), np.amin(field2[:,:,x]) )
+    flds_max_value = max( np.amax(field1[:,:,x]), np.amax(field2[:,:,x]) )
+
+    diff_min_value = -max( abs(np.amin(field1[:,:,x]-field2[:,:,x])), abs(np.amax(field1[:,:,x]-field2[:,:,x])) )
+    diff_max_value =  max( abs(np.amin(field1[:,:,x]-field2[:,:,x])), abs(np.amax(field1[:,:,x]-field2[:,:,x])) )
+ 
+    fig = plt.figure(figsize=(9,15))
+    ax1 = plt.subplot(311)
+    ax2 = plt.subplot(312)
+    ax3 = plt.subplot(313)
+    ax1, im1 = plot_x_crss_sec_ax(ax1, field1, x, flds_min_value, flds_max_value)
+    ax2, im2 = plot_x_crss_sec_ax(ax2, field2, x, flds_min_value, flds_max_value)
+    ax3, im3 = plot_x_crss_sec_ax(ax3, field1-field2, x, diff_min_value, diff_max_value, cmap='bwr')
+
+    ax1.set_title(str(field1_name)+' at x='+str(x))
+    ax2.set_title(str(field2_name)+' at x='+str(x))
+    ax3.set_title('The Difference')
+
+    cb1axes = fig.add_axes([0.92, 0.42, 0.03, 0.52]) 
+    cb3axes = fig.add_axes([0.92, 0.08, 0.03, 0.20]) 
+    cb1=plt.colorbar(im1, ax=(ax1,ax2), orientation='vertical', cax=cb1axes)
+    cb3=plt.colorbar(im3, ax=ax3, orientation='vertical', cax=cb3axes)
+ 
+    if title:
+       plt.suptitle(title, fontsize=14)
+
+    plt.tight_layout()
+    plt.subplots_adjust(hspace = 0.5, right=0.9, bottom=0.07, top=0.95)
+
+    return(fig)
+
+#######################################
+# Plot time series at specific points #
+#######################################
+
+def plt_timeseries_ax(ax, point, length, datasets, ylim=None):
+
+   my_legend=[]
+   for name, dataset in datasets.items():
+      print(name)
+      print(dataset.shape)
+      #ii = np.argwhere(dataset[:, point[0], point[1], point[2]]==0)
+      ii = np.argwhere(np.isnan(dataset[:, point[0], point[1], point[2]]))
+      if ii.shape[0]==0:
+         end=length
+      else: 
+         end=min(np.amin(ii),length)
+      print(end)
+      ax.plot(dataset[:end, point[0], point[1], point[2]])
+      my_legend.append(name)
+   ax.legend(my_legend)
+   ax.set_ylabel('Temperature')
+   ax.set_xlabel('No of months')
+   ax.set_title(str(int(length/12))+' years')
+   if ylim:
+      ax.set_ylim(ylim)
+ 
+   return(ax)
+
+def plt_timeseries(point, length, datasets, ylim=None):
+   
+   fig = plt.figure(figsize=(15 ,3))
+
+   ax=plt.subplot(111)
+   ax=plt_timeseries_ax(ax, point, length, datasets, ylim=ylim)
+
+   plt.tight_layout()
+   plt.subplots_adjust(hspace = 0.5, left=0.05, right=0.95, bottom=0.15, top=0.90)
+
+   return(fig)
+
+def plt_3_timeseries(point, length1, length2, length3, datasets, ylim=None):
+   
+   fig = plt.figure(figsize=(15 ,11))
+
+   ax=plt.subplot(311)
+   ax=plt_timeseries_ax(ax, point, length1, datasets, ylim=ylim)
+   ax=plt.subplot(312)
+   ax=plt_timeseries_ax(ax, point, length2, datasets, ylim=ylim)
+   ax=plt.subplot(313)
+   ax=plt_timeseries_ax(ax, point, length3, datasets, ylim=ylim)
+
+   plt.tight_layout()
+   plt.subplots_adjust(hspace = 0.5, left=0.05, right=0.95, bottom=0.07, top=0.95)
+
+   return(fig)
+
+###############################
+# Plot histogram of y_hat - y #
+###############################
+def Plot_Histogram(y, y_hat, no_bins)
+
+    fig = plt.figure(figsize=(10, 8))
+
+    y_minus_y_hat = y - y_hat
+    plt.hist(x, bins = no_bins)
+    ########
+    ###annotate with skew(x)
+    return(fig)
